564c88d8b358f85fb4fe1b822f19d671befd22cc
Roman Boyko <roman_boyko.48@mail.ru>
2022-10-24 23:58:53 +0300

    ready to review

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -10,24 +10,8 @@ class DynamicUrlTests(TestCase):
         response = Client().get('/catalog/0')
         self.assertEqual(response.status_code, 404)
 
-    def test_underscore(self):
-        response = Client().get('/catalog/_')
-        self.assertEqual(response.status_code, 404)
-
-    def test_number_contains_string(self):
-        response = Client().get('/catalog/str')
-        self.assertEqual(response.status_code, 404)
-
-    def test_number_with_ubderscore_and_string(self):
-        response = Client().get('/catalog/str_123')
-        self.assertEqual(response.status_code, 404)
-
-    def test_string_and_numbers(self):
-        response = Client().get('/catalog/123str')
-        self.assertEqual(response.status_code, 404)
-
-    def test_big_number_with_leading_zeroes(self):
-        response = Client().get('/catalog/003456')
+    def test_number_starts_with_two_zeroes(self):
+        response = Client().get('/catalog/00')
         self.assertEqual(response.status_code, 404)
 
     def test_number_starts_with_zero(self):
@@ -38,24 +22,24 @@ class DynamicUrlTests(TestCase):
         response = Client().get('/catalog/001')
         self.assertEqual(response.status_code, 404)
 
-    def test_number_starts_with_two_zeroes(self):
-        response = Client().get('/catalog/00')
-        self.assertEqual(response.status_code, 404)
-
-    def test_number_starts_with_underscore(self):
-        response = Client().get('/catalog/_01')
-        self.assertEqual(response.status_code, 404)
-
-    def test_strings_and_then_numbers(self):
-        response = Client().get('/catalog/str123')
+    def test_big_number_with_leading_zeroes(self):
+        response = Client().get('/catalog/003456')
         self.assertEqual(response.status_code, 404)
 
     def test_string_with_leading_zeroes(self):
         response = Client().get('/catalog/00str')
         self.assertEqual(response.status_code, 404)
 
-    def test_number_contains_string_inside(self):
-        response = Client().get('/catalog/123str456')
+    def test_underscore(self):
+        response = Client().get('/catalog/_')
+        self.assertEqual(response.status_code, 404)
+
+    def test_number_starts_with_underscore(self):
+        response = Client().get('/catalog/_01')
+        self.assertEqual(response.status_code, 404)
+
+    def test_big_number_starts_with_underscore(self):
+        response = Client().get('/catalog/_456789')
         self.assertEqual(response.status_code, 404)
 
     def test_number_contains_underscore(self):
@@ -64,6 +48,26 @@ class DynamicUrlTests(TestCase):
 
     def test_underscope_at_the_end_of_number(self):
         response = Client().get('/catalog/123_')
+        self.assertEqual(response.status_code, 404)
+
+    def test_number_contains_string(self):
+        response = Client().get('/catalog/str')
+        self.assertEqual(response.status_code, 404)
+
+    def test_strings_and_then_numbers(self):
+        response = Client().get('/catalog/str123')
+        self.assertEqual(response.status_code, 404)
+
+    def test_string_and_numbers(self):
+        response = Client().get('/catalog/123str')
+        self.assertEqual(response.status_code, 404)
+
+    def test_number_contains_string_inside(self):
+        response = Client().get('/catalog/123str456')
+        self.assertEqual(response.status_code, 404)
+
+    def test_number_with_ubderscore_and_string(self):
+        response = Client().get('/catalog/str_123')
         self.assertEqual(response.status_code, 404)
 
     def test_float_numbers_that_equals_to_integer(self):
