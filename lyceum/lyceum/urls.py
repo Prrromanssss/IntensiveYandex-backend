ff8554ce82e6351bd0ccb4a828ca322836a3f4dd
Roman Boyko <roman_boyko.48@mail.ru>
2022-10-25 00:58:27 +0300

    ready to review

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -19,8 +19,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('homepage.urls')),
-    path('catalog/', include('catalog.urls')),
-    path('about/', include('about.urls')),
+    path('', include('homepage.urls', namespace='homepage')),
+    path('catalog/', include('catalog.urls', namespace='catalog')),
+    path('about/', include('about.urls', namespace='about')),
     path('admin/', admin.site.urls),
 ]
