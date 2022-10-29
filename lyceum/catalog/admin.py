dbea3ad0ff4314337fb6df829de9a2030b7b87c5
Roman Boyko <roman_boyko.48@mail.ru>
2022-10-29 19:40:32 +0300

    made half of models of catalog admin

--- lyceum/catalog/admin.py
+++ lyceum/catalog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from .models import Item, Tag, Category
 
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published')
+    list_editable = ('is_published',)
+    list_display_links = ('name',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published')
+    list_editable = ('is_published',)
+    list_display_links = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published')
+    list_editable = ('is_published',)
+    list_display_links = ('name',)
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Category, CategoryAdmin)
