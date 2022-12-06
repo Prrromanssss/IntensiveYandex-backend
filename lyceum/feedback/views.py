0840cc73b8f51483821553fdcae9776a07b27b2e
Sourr_cream <roman_boyko.48@mail.ru>
2022-12-06 10:13:59 +0300

    cahnge feedback form

--- lyceum/feedback/views.py
+++ lyceum/feedback/views.py
@@ -12,15 +12,10 @@ class FeedbackView(FormView):
     model = Feedback
     form_class = FeedbackForm
     success_url = reverse_lazy('feedback:feedback')
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['form'] = FeedbackForm
-        return context
+    context_object_name = 'form'
 
     def post(self, request, *args, **kwargs):
-
-        form = FeedbackForm(request.POST or None)
+        form = self.form_class(request.POST or None)
 
         if form.is_valid():
             name = form.cleaned_data['name']
@@ -42,45 +37,9 @@ class FeedbackView(FormView):
                 fail_silently=False,
             )
 
-            Feedback.objects.create(
+            self.model.objects.create(
                 **form.cleaned_data
             )
             return redirect(self.get_success_url())
 
-        return render(request, self.template_name, self.get_context_data())
-
-
-def feedback(request):
-    template_name = 'feedback/feedback.html'
-    form = FeedbackForm(request.POST or None)
-    context = {
-        'form': form,
-    }
-
-    if request.method == 'POST' and form.is_valid():
-        name = form.cleaned_data['name']
-        text = form.cleaned_data['text']
-        mail = form.cleaned_data['mail']
-        message = (
-            f'Здравствуйте, {name}! '
-            'Вы получили это письмо, так как написали в Boyko Company.\n'
-            f'Ваш отзыв:\n{text}\n\n'
-            'Спасибо за фидбэк! Мы обязательно учтем ваше мнение.\n'
-            '© Boyko Company'
-        )
-
-        send_mail(
-            'Boyko company',
-            message,
-            settings.ADMIN_EMAIL,
-            [mail],
-            fail_silently=False,
-        )
-
-        Feedback.objects.create(
-            **form.cleaned_data
-        )
-
-        return redirect('feedback:feedback')
-
-    return render(request, template_name, context)
+        return render(request, self.template_name)
