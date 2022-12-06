172a69cd43adfa498fd48d88bae51fae11e2d94a
Sourr_cream <roman_boyko.48@mail.ru>
2022-12-06 19:48:02 +0300

    cbv of item detail have done

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -1,10 +1,11 @@
 from catalog.models import Item
 from django.db.models import Avg
-from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import FormMixin
 from rating.forms import RatingForm
 from rating.models import Rating
-from users.models import CustomUser
 
 
 class ItemListView(ListView):
@@ -16,34 +17,54 @@ class ItemListView(ListView):
         return Item.objects.published().order_by('category__name', 'name')
 
 
-class ItemDetailView(DetailView):
+class ItemDetailView(FormMixin, DetailView):
     model = Item
+    form_model = Rating
     template_name = 'catalog/view_element.html'
     context_object_name = 'item'
-
-    def get_queryset(self):
-        return Item.objects.published()
+    form_class = RatingForm
+    get_queryset = Item.objects.published
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-
-        context['form'] = RatingForm(self.request.POST or None)
-        context['grade'] = Rating.objects.filter(
+        context['form'] = self.form_class(self.request.POST or None)
+        context['grade'] = self.form_model.objects.filter(
             user_id=self.request.user.id,
             item_id=self.kwargs['pk']
         )
-        context['number'] = Rating.objects.filter(
+        context['number'] = self.form_model.objects.filter(
             item_id=self.kwargs['pk']
         ).count()
-        context['average'] = Rating.objects.filter(
+        context['average'] = self.form_model.objects.filter(
             item_id=self.kwargs['pk']
         ).aggregate(
             Avg('grade')
         )['grade__avg']
-        if self.request.user.is_authenticated:
-            context['user'] = get_object_or_404(
-                CustomUser,
-                id=self.request.user.id
-            )
 
         return context
+
+    def get_success_url(self, **kwargs):
+        if kwargs is not None:
+            return reverse_lazy(
+                'catalog:item_detail',
+                kwargs={'pk': kwargs['pk']}
+            )
+        return reverse_lazy('catalog:item_detail')
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(
+            request.POST or None,
+            instance=request.user,
+        )
+        if form.is_valid():
+            if not self.form_model.objects.filter(
+                    user_id=request.user.id,
+                    item_id=self.kwargs['pk']
+            ).update(**form.cleaned_data):
+                self.form_model.objects.create(
+                    **form.cleaned_data,
+                    user_id=request.user.id,
+                    item_id=self.kwargs['pk']
+                )
+            return redirect(self.get_success_url(**self.kwargs))
+        return render(request, self.template_name, self.get_context_data())
